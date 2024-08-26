f0372e08389ea4098b6a758493c5b9ec8f5f9c6f
awj1052 <awj1052@naver.com>
2024-08-27 04:19:49 +0900

    update scrapping

--- db.py
+++ db.py
@@ -1,16 +1,66 @@
-import os, pymysql
+import os, pymysql, datetime
 from dotenv import load_dotenv
+load_dotenv()
 
-def _init():
-    load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_DATABASE = os.getenv("DB_DATABASE")
 
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = int(os.getenv("DB_PORT"))
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_DATABASE = os.getenv("DB_DATABASE")
+conn = pymysql.connect(host=DB_HOST, user=DB_USER, passwd = DB_PASSWORD, db=DB_DATABASE, port=DB_PORT)
 
-    conn = pymysql.connect(host=DB_HOST, user=DB_USER, passwd = DB_PASSWORD, db=DB_DATABASE, port=DB_PORT)
-    return conn.cursor()
+_cursor = conn.cursor()
 
-cursor = _init()
+def close():
+    _cursor.close()
+    conn.close()
+
+def get_user(username=''):
+    if username == '':
+        sql = "SELECT * FROM user"
+        rows = _cursor.execute(sql)
+    else:
+        sql = "SELECT * FROM user WHERE name = %s"
+        rows = _cursor.execute(sql, username)
+    return _cursor.fetchall()
+
+def update_user(username, corrects, submissions, solution):
+    sql = "SELECT * FROM user WHERE name = %s"
+    rows = _cursor.execute(sql, (username))
+    if rows == 0:
+        sql = "INSERT INTO user (corrects, submissions, solution, name) VALUES (%s, %s, %s, %s)"
+    else:
+        sql = "UPDATE user SET corrects = %s, submissions = %s, solution = %s WHERE name = %s"
+    rows = _cursor.execute(sql, (corrects, submissions, solution, username))
+    conn.commit()
+    return rows
+
+# time = "2024-08-25 19:52:59"
+# time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+def add_problem(username, problem_id, time):
+    sql = "SELECT * FROM problem WHERE name = %s AND problem = %s"
+    rows = _cursor.execute(sql, (username, problem_id))
+    if rows != 0:
+        sql = "UPDATE problem SET time = %s WHERE name = %s AND problem = %s"
+        rows = _cursor.execute(sql, (time, username, problem_id))
+    else:
+        sql = "INSERT INTO problem (name, problem, time) VALUES (%s, %s, %s)"
+        rows = _cursor.execute(sql, (username, problem_id, time))
+    conn.commit()
+    return rows
+
+def get_problems(username):
+    sql = "SELECT * FROM problem WHERE name = %s"
+    rows = _cursor.execute(sql, (username))
+    return _cursor.fetchall()
+
+def get_users_by_problem(problem_id):
+    sql = "SELECT * FROM problem WHERE problem = %s"
+    rows = _cursor.execute(sql, (problem_id))
+    return _cursor.fetchall()
+
+# print(update_user("qwe", 12, 2, 3))
+# print(get_user("awj1052"))
+# print(add_problem("awj1052", 2, datetime.datetime.now()))
+# print(get_problems("awj1052"))
--- user_info.py
+++ user_info.py
@@ -1,11 +1,13 @@
-import requests, os
+import requests, os, datetime, time
 from dotenv import load_dotenv
 load_dotenv()
 
 USER_INFO = os.getenv("USER_INFO")
 USER_SUBMITTION = os.getenv("USER_SUBMITTION")
 
-def solved_problems(username):
+# for init
+def solved_problems(username, key):
+    if key != "init": return []
     response = requests.get(
         url = f'{USER_INFO}/{username}',
         headers = {
@@ -21,7 +23,25 @@ def solved_problems(username):
     problems = list(map(int,html.split()))
     return problems
 
-def recent_solved_problems(username, last_submit, query=''):
+# time = "2024-08-25 19:52:59"
+# time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+def str2datetime(s='epoch'):
+    try:
+        return datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+    except:
+        return datetime.datetime.fromtimestamp(0) # epoch time
+
+called = 0
+def recent_solved_problems(username, last_solution):
+    global called
+    called = 0
+    return _recent_solved_problems(username, last_solution)
+
+def _recent_solved_problems(username, last_solution, query=''):
+    global called
+    called += 1
+    if called > 5:
+        return []
     response = requests.get(
         url = f'{USER_SUBMITTION}user_id={username}&{query}',
         headers = {
@@ -31,4 +51,35 @@ def recent_solved_problems(username, last_submit, query=''):
 
     html = response.text
 
+    table = html.find('status-table">') + 14
+    table = html.find('<tbody>', table) + 7
+    table_end = html.find('</tbody>', table)
+    html = html[table:table_end].split('</tr>')
+    html.pop()
+
+    data = []
+    solution = 0
+    flag = 1
+    for line in html:
+        try:
+            elements = line.split('</td>')
+            solution = int(elements[0].split('<td>')[1])
+            if solution <= last_solution:
+                flag = 0
+                break
+            problem_id = int(elements[2].split('" rel')[0].replace('<td><a href="/problem/', ''))
+            time_value = str2datetime(elements[8].split('" data-timestamp')[0].split('title="')[1])
+            data.append((solution, problem_id, time_value))
+        except:
+            continue
+    if flag:
+        time.sleep(0.05)
+        data = data + _recent_solved_problems(username, last_solution, 'top=' + str(solution-1))
+    return data
     
+
+data = recent_solved_problems("awj1052", 82340610)
+print(len(data))
+print(*data, sep='\n')
+# print(str2datetime("2024-08-25 19:52:59"))
+
